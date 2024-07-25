a0c27180204fbce4719cc9222a6a4faa150c0d3b
Juan Zapana <juanzapanacondori@gmail.com>
2024-07-25 10:56:50 -0400

    Rutas y variables de entorno

---
+++ src/products/products.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+
+@Controller('products')
+export class ProductsController {
+  constructor() { }
+
+  @Post()
+  CreateProduct() {
+    return 'Crea un producto';
+  }
+
+  @Get()
+  findAllProducts() {
+    return 'Lista de productos';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return 'Esta funcion regresa el producto ' + id;
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return 'Esta funcion elimina el producto ' + id;
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body() body: any
+  ) {
+    return 'Esta funcion actualiza el producto ';
+  }
+
+
+}
